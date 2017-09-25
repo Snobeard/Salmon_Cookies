2ea9a254244b0e2f57fdf4f13757656c87ed74d3
Snobeard <fatmattmatt@gmail.com>
2017-09-25 15:40:47 -0700

    added seattle center and capital hill

--- app.js
+++ app.js
@@ -9,56 +9,142 @@ var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm
 var firstPike = {
   minCustomers: 23,
   maxCustomers: 65,
-  cookiePerCystomer: 6.3,
+  cookiePerCustomer: 6.3,
   cookiesArray: [],
-  randomCustomers: function () {
+  randomCustomers: function () { // generates a random number between the given minimum and maximum customers
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  averageCookieSold: function() { // gives the random number of cookies based on the number generated for customers
+    var cookies = Math.round(this.randomCustomers() * this.cookiePerCustomer);
+    return cookies;
+  },
+  getCookies: function() {
+    this.cookiesArray = []; // resets cookiesArray to an empty array before initializing a new array
+    for (var i in openHours) {
+      var cookiesSold = this.averageCookieSold(); // runs the method for random cookies
+      var liElement = document.createElement('li'); // creates the <li></li> element
+      liElement.textContent = openHours[(i)] + ': ' + cookiesSold + ' cookies.'; // creates content for the list element
+      var pikeUl = document.getElementById('pike'); // chooses list determined upon the id
+      pikeUl.appendChild(liElement); // attaches the given content at the end of the list
+      this.cookiesArray.push(cookiesSold); // adds the random cookies generated to an array
+    };
+    var total = 0; // gives the sum of the cookie array
+    for (var j = 0; j < this.cookiesArray.length; j ++) {
+      total = total + this.cookiesArray[j]; // adds the next cookie in the array to the total
+    };
+    var tally = document.createElement('li');
+    tally.textContent = 'Total: ' + total; // prints out the total in a list below the list of hours
+    pikeUl.appendChild(tally); // attaches the given content at the end of the list chosen above
+  }
+};
+
+var seaTac = {
+  minCustomers: 3,
+  maxCustomers: 24,
+  cookiePerCustomer: 1.2,
+  cookiesArray: [],
+  randomCustomers: function() {
     var min = Math.ceil(this.minCustomers);
     var max = Math.floor(this.maxCustomers);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   },
   averageCookieSold: function() {
-    var cookies = Math.round(this.randomCustomers() * this.cookiePerCystomer);
+    var cookies = Math.round(this.randomCustomers() * this.cookiePerCustomer);
     return cookies;
   },
   getCookies: function() {
     this.cookiesArray = [];
     for (var i in openHours) {
       var cookiesSold = this.averageCookieSold();
-      var liEl = document.createElement('li');
-      liEl.textContent = openHours[i] + ': ' + cookiesSold + ' cookies.';
-      var pikeUl = document.getElementById('pike');
-      pikeUl.appendChild(liEl);
+      var liElement = document.createElement('li');
+      liElement.textContent = openHours[i] + ': ' + cookiesSold + ' cookies.';
+      var seaTacUl = document.getElementById('airport');
+      seaTacUl.appendChild(liElement);
       this.cookiesArray.push(cookiesSold);
     };
     var total = 0;
     for (var j = 0; j < this.cookiesArray.length; j ++) {
       total = total + this.cookiesArray[j];
     }
-    liEl.textContent = 'Total Cookies: ' + total;
-    pikeUl.appendChild(liEl);
+    var tally = document.createElement('li');
+    tally.textContent = 'Total: ' + total;
+    seaTacUl.appendChild(tally);
   }
-};
-
-var seaAirport = {
-  minCustomers: 3,
-  maxCustomers: 24,
-  averageCookie: 1.2
 };
 
 var seaCenter = {
   minCustomers: 11,
   maxCustomers: 38,
-  averageCookie: 3.7
+  cookiePerCustomer: 3.7,
+  cookiesArray: [],
+  randomCustomers: function() {
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min - 1)) + min;
+  },
+  averageCookieSold: function() {
+    var cookies = Math.round(this.randomCustomers() * this.cookiePerCustomer);
+    return cookies;
+  },
+  getCookies: function() {
+    this.cookiesArray = [];
+    for (var i in openHours) {
+      var cookiesSold = this.averageCookieSold();
+      var liElement = document.createElement('li');
+      liElement.textContent = openHours[i] + ': ' + cookiesSold + ' cookies.';
+      var seaCenterUl = document.getElementById('seaCenter');
+      seaCenterUl.appendChild(liElement);
+      this.cookiesArray.push(cookiesSold);
+    };
+    var total = 0;
+    for (var j = 0; j < this.cookiesArray.length; j ++) {
+      total = total + this.cookiesArray[j];
+    }
+    var tally = document.createElement('li');
+    tally.textContent = 'Total: ' + total;
+    seaCenterUl.appendChild(tally);
+  }
 };
 
 var capHill = {
   minCustomers: 20,
   maxCustomers: 38,
-  averageCookie: 2.3
+  cookiePerCustomer: 2.3,
+  cookiesArray: [],
+  randomCustomers: function() {
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min - 1)) + min;
+  },
+  averageCookieSold: function() {
+    var cookies = Math.round(this.randomCustomers() * this.cookiePerCustomer);
+    return cookies;
+  },
+  getCookies: function() {
+    this.cookiesArray = [];
+    for (var i in openHours) {
+      var cookiesSold = this.averageCookieSold();
+      var liElement = document.createElement('li');
+      liElement.textContent = openHours[i] + ': ' + cookiesSold + ' cookies.';
+      var capHillUL = document.getElementById('capHill');
+      capHillUL.appendChild(liElement);
+      this.cookiesArray.push(cookiesSold);
+    };
+    var total = 0;
+    for (var j = 0; j < this.cookiesArray.length; j ++) {
+      total = total + this.cookiesArray[j];
+    }
+    var tally = document.createElement('li');
+    tally.textContent = 'Total ' + total;
+    capHillUL.appendChild(tally);
+  }
 };
 
 var alki = {
   minCustomers: 2,
   maxCustomers: 16,
-  averageCookie: 4.6
+  cookiePerCustomer: 4.6,
+  cookiesArray: []
 };
