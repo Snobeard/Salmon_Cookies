eada2478188270b7155e074ad6e79f358e4597fd
Snobeard <fatmattmatt@gmail.com>
2017-09-25 13:14:33 -0700

    setting up location objects

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict';
+// 6AM to 8PM
+// min number of customers per hour
+// max number of customers per hour
+// average number of cookies purchased per customer
+
+var firstPike = {
+  minCustomers: 23,
+  maxCustomers: 65,
+  averageCookie: 6.3
+};
+
+var seaAirport = {
+  minCustomers: 3,
+  maxCustomers: 24,
+  averageCookie: 1.2
+};
+
+var seaCenter = {
+  minCustomers: 11,
+  maxCustomers: 38,
+  averageCookie: 3.7
+};
+
+var capHill = {
+  minCustomers: 20,
+  maxCustomers: 38,
+  averageCookie: 2.3
+};
+
+var alki = {
+  minCustomers: 2,
+  maxCustomers: 16,
+  averageCookie: 4.6
+};
