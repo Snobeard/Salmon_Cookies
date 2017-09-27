1904a1fb676cf2577426c8e0f0075a2399c1faef
Snobeard <fatmattmatt@gmail.com>
2017-09-27 12:46:14 -0700

    resubmit, setting table in both html's

--- js/sales.js
+++ js/sales.js
@@ -1,7 +1,9 @@
 'use strict';
 
 // // sets up web page
+locations[0].addHeader();
 for (var i in locations) {
   locations[i].getCookies();
+  locations[i].tableCookies();
   locations[i].listCookies();
 };
